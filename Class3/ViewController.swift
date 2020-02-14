f393ce2b494ae68bc5552d0bc8ad878169321162
Dioh <dioh.desush@icloud.com>
2020-02-14 09:28:35 -0500

    Initial Commit

---
+++ Class3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class3
+//
+//  Created by Dioh Desuah on 2/14/20.
+//  Copyright © 2020 Dioh Desuah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
